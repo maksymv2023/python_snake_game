d88cd0ef333da83660911834131d1661584cee90
MadMaksym <maksym.voitovych@outlook.com>
2024-01-31 20:40:39 -0500

    git commit -m "control the snake"

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
+
 screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor("black")
@@ -7,17 +9,14 @@ screen.title("My Snake Game")
 screen.tracer(0)
 # 1. Create a snake
 
-starting_position = [(0,0), (-20,0), (-40,0)]
 
-segments = []
+snake = Snake()
 
-for position in starting_position:
-    new_segment = Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 # 2. Move the snake automatically
 game_is_on = True
@@ -25,11 +24,9 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+    snake.move()
+
+
 
 
 
