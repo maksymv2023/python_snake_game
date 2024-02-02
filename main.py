90b4c969d82089117ec35b9a181000adee83569f
MadMaksym <maksym.voitovych@outlook.com>
2024-02-01 19:28:37 -0500

    git commit -m "control the snake"

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
+from score_board import ScoreBoard
 import time
 
 screen = Screen()
@@ -11,6 +13,8 @@ screen.tracer(0)
 
 
 snake = Snake()
+food = Food()
+score = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -23,19 +27,12 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
-
-
-
-
-
-
-
-
-
-
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        score.increase_score()
 
 
 
